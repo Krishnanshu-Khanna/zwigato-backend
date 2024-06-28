4d82d850bcf8d2c5bdc56083a404650d1d85ae3a
Krishnanshu <krishnanshu.khanna@gmail.com>
2024-06-29 00:02:49 +0530

    feat: Add CORS headers middleware
    This commit adds a middleware function to set the necessary CORS headers for the server. It allows requests from the specified origin, sets the allowed methods and headers, enables credentials and private network access, and sets the maximum age for preflight requests. This change improves the server's security and compatibility with cross-origin requests.

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,26 @@ mongoose
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use((req, res, next) => {
+	res.setHeader(
+		"Access-Control-Allow-Origin",
+		"https://zwigato-frontend.onrender.com/"
+	);
+	res.setHeader(
+		"Access-Control-Allow-Methods",
+		"GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+	);
+	res.setHeader(
+		"Access-Control-Allow-Headers",
+		"Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+	);
+	res.setHeader("Access-Control-Allow-Credentials", "true");
+	res.setHeader("Access-Control-Allow-Private-Network", "true");
+	//  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+	res.setHeader("Access-Control-Max-Age", 7200);
+
+	next();
+});
 
 app.get("/health", async(req:Request, res:Response) => {
 	res.send({ message: "Server is working fine health Ok!" });
